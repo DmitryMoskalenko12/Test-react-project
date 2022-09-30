10bed9cb076b155bb839935af99cc543f7f097c7
Dima <dmtrmsklnk@gmail.com>
2022-10-01 00:44:14 +0300

    start test project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Counter from "./components/Counter";
+import PostItem from "./components/PostItem";
+import './styles/App.scss';
+import { useState } from "react";
+import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
 function App() {
+const [posts, setPosts] = useState([
+  {id: 1, title: 'JavaScript', body: 'Description'},
+  {id: 2, title: 'JavaScript 2', body: 'Description'},
+  {id: 3, title: 'JavaScript 3', body: 'Description'}
+])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <form>
+        <input type="text" placeholder="Назва посту"/>
+        <input type="text" placeholder="Опис посту"/>
+        <MyButton disabled >Створити пост</MyButton>
+      </form>
+      <PostList posts = {posts} title = {'Список постів 1'}/>
     </div>
-  );
+  )
 }
 
 export default App;
